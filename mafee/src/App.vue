7b28a2a7f9691c8a88c7b061c0ac579fd5bc2502
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-12-21 19:49:46 -0300

    Update

--- mafee/src/App.vue
+++ mafee/src/App.vue
@@ -2,7 +2,6 @@
 import { RouterView } from 'vue-router'
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
