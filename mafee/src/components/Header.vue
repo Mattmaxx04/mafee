90eaba7946e530338a728e8bf80d63c360a9e3bd
Mattmaxx04 <95719090+Mattmaxx04@users.noreply.github.com>
2022-12-21 20:11:17 -0300

    Header

--- mafee/src/components/Header.vue
+++ mafee/src/components/Header.vue
@@ -3,9 +3,15 @@ import Nav from './Nav.vue';
 </script>
 
 <template>
+    <div class="header__container">
     <Nav />
+</div>
 </template>
 
 <style scoped>
-
+.header__container{
+    background: var(--color-btn-dark);
+    height: 10vh;
+    width: 100%;
+}
 </style>
--- mafee/src/components/Nav.vue
+++ mafee/src/components/Nav.vue
@@ -3,6 +3,7 @@ import { RouterLink} from 'vue-router'
 </script>
 
 <template>
+    <div class="nav__links">
             <RouterLink to="/"> Home </RouterLink>
             <RouterLink to="/about"> About us </RouterLink>
             <RouterLink to="/blogmain"> Blog </RouterLink>
@@ -12,9 +13,19 @@ import { RouterLink} from 'vue-router'
             <RouterLink to="/reservation"> Reservation </RouterLink>
             <RouterLink to="/shop"> Shop </RouterLink>
             <RouterLink to="/workwithus"> Work with us  </RouterLink>
-
+        </div>
 </template>
 
 <style scoped>
-
+a{
+    text-decoration: none;
+    
+}
+.nav__links{
+    
+    display: flex;
+    background: transparent;
+    justify-content: space-around;
+    align-items: center;
+}
 </style>
