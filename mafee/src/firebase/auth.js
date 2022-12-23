496e4074fe36361d2819f54a745749547c3e354f
marcosarfa <marcosarfa@gmail.com>
2022-12-23 19:25:16 -0300

    login face y mail

--- mafee/src/firebase/auth.js
+++ mafee/src/firebase/auth.js
@@ -1,35 +1,72 @@
 import { app } from "./index.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import user from "../store/user.js";
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 function loginWithGoogle() {
-    signInWithPopup(auth, provider)
-    .then((result)=>{;
-        let newUser = {
-            displayName: result.user.displayName,
-            photoURL: result.user.photoURL,
-            email: result.user.email,
-        }
-        localStorage.setItem('user',JSON.stringify(newUser))
-        user.value = {
-            ...newUser
-        }
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      let newUser = {
+        displayName: result.user.displayName,
+        photoURL: result.user.photoURL,
+        email: result.user.email,
+      };
+      localStorage.setItem("user", JSON.stringify(newUser));
+      user.value = {
+        ...newUser,
+      };
     })
-    .catch((error)=>{
-        console.warn('error', error);
-    })
+    .catch((error) => {
+      console.warn("error", error);
+    });
 }
 
-function logout(){
-    signOut(auth).then(() => {
-        user.value = null;
-        localStorage.setItem('user',null)
-      }).catch((error) => {
-
-      });
+function logout() {
+  signOut(auth)
+    .then(() => {
+      user.value = null;
+      localStorage.setItem("user", null);
+    })
+    .catch((error) => {});
 }
 
-export { loginWithGoogle, logout }
+//Registro de Usuario con Mail y Pass
+
+function register() {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+}
+
+//login con mail y pass
+function loginWithMail() {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+
+export { loginWithGoogle, logout, register };
