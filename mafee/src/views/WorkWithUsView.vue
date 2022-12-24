9d54938a59d77ec62a3b607458d11c5e5784b737
marcosarfa <marcosarfa@gmail.com>
2022-12-23 21:42:08 -0300

    work with us

--- mafee/src/views/WorkWithUsView.vue
+++ mafee/src/views/WorkWithUsView.vue
@@ -3,10 +3,75 @@
 </script>
 
 <template>
+    <div class="home__discover text-center">
+        <img class="img__start" src="../assets/Team-title-img.jpg" alt="">
+    </div>
+    <div class="d-flex justify-content-center">
+        <img class="img__team" src="../assets/Home-4-img-gallery-1.jpg" alt="">
+        <img class="img__team" src="../assets/Home-4-img-gallery-2.jpg" alt="">
+        <img class="img__team" src="../assets/Home-4-img-gallery-3.jpg" alt="">
+    </div>
+    <div class="d-flex justify-content-center">
+        <img class="img__team" src="../assets/Home-4-img-gallery-4.jpg" alt="">
+        <img class="img__team" src="../assets/Home-4-img-gallery-5.jpg" alt="">
+        <img class="img__team" src="../assets/Home-4-img-gallery-6.jpg" alt="">
+    </div>
 
+    <div class="meet__process text-center">
+        <div>
+            <img src="../assets/Shop-home-video-img.jpg" alt="">
+        </div>
+        <div>
+            <div class="d-flex flex-column">
+                <div class="d-flex">
+                    <img src="../assets/about-home-04-icon-3.png" alt="">
+                    <div>
+                        <h3>BREWERY PROCESS</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae sed iusto sapiente ex nemo voluptate, in neque modi, at laudantium accusamus. Voluptatum maiores quia saepe eum eveniet eaque. Quibusdam, qui.</p>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <img src="../assets/about-home-04-icon-7.png" alt="">
+                    <div>
+                        <h3>CRAFT BEER</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quas hic libero accusantium tenetur facilis quibusdam velit, repudiandae officiis pariatur est voluptates earum vel nihil corrupti aspernatur, iste sit dolorum.</p>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <img src="../assets/about-home-04-icon-6.png" alt="">
+                    <img src="../assets/about-home-04-icon-6.png" alt="">
+                    <div>
+                        <h3>PUB SUPPLY</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, est? Nisi dolore sint eos at! Maxime tenetur, quam, aliquam, ad temporibus reprehenderit non sint optio incidunt delectus voluptatum nobis eligendi!</p>
+                    </div>
+                </div>
+            </div>
+            <div>
 
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.home__discover {
+    padding: 1.5rem;
+    margin-top: 3rem;
+}
+
+.meet__process{
+    width: 95vw;
+    display: flex;
+    justify-content: center;
+}
+
+.img__start {
+    width: 95vw;
+}
+
+.img__team {
+    padding: 1.5rem;
+    width: 39rem;
+}
 
 </style>
