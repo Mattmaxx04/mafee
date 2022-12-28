50fb80668ebf13b990adbba538e5e23bbad22b71
Ezequiel Gomez <juanezequielgomez95@gmail.com>
2022-12-28 16:07:32 -0300

    Update MyAccount

--- mafee/src/components/Location.vue
+++ mafee/src/components/Location.vue
@@ -11,37 +11,41 @@ import { ref, computed } from 'vue';
     <div class="location">
         <div class="tab-content p-4 p-md-5">
             <div class="tab-pane fade active show" id="account" role="tabpanel" aria-labelledby="account-tab">
-                <h3 class="mb-4">Location</h3>
-                <div class="row">
+                <div class="title2">
+            <h2 class="text-center">LOCATION</h2>
+        </div>
+                <div class="row form_main">
 
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Address</label>
-                            <input type="text" class="form-control" value="Rivadavia 3882">
+                    <div class="form">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Address</label>
+                                <input type="text" class="form-control" value="Rivadavia 3882">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>City</label>
-                            <input type="text" class="form-control" value="Buenos Aires">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>City</label>
+                                <input type="text" class="form-control" value="Buenos Aires">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>State</label>
-                            <input type="text" class="form-control" value="Buenos Aires">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>State</label>
+                                <input type="text" class="form-control" value="Buenos Aires">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Postal code</label>
-                            <input type="text" class="form-control" value="1407">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Postal code</label>
+                                <input type="text" class="form-control" value="1407">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Phone number</label>
-                            <input type="text" class="form-control" value="1532313164">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Phone number</label>
+                                <input type="text" class="form-control" value="1532313164">
+                            </div>
                         </div>
                     </div>
                   
@@ -60,6 +64,52 @@ import { ref, computed } from 'vue';
 
 <style scoped>
 .account__btns{
-    margin: 0.5rem;
+    /* margin: 0.5rem!important; */
+    margin-left: 55rem !important;
+}
+
+.btn-dark{
+    margin-right: 2rem !important;
+}
+.title2{
+    background-image: url(../assets/background-img-color.jpg);
+    padding: 2rem 1rem 2rem 1rem;
+    margin: 1rem 15rem 4rem 15rem;
+    border-radius: 1rem;
+}
+
+.title2 h2{
+    font-weight: bold;
+    font-size: 2.2rem;
+    color: var(--color-btn);
+}
+
+.location{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 3rem;
+}
+
+.form_main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 3rem;
+}
+
+.form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+    width: 50rem;
+}
+
+label{
+    padding-bottom: 1rem;
 }
 </style>
--- mafee/src/components/Profile.vue
+++ mafee/src/components/Profile.vue
@@ -12,34 +12,38 @@ const props = defineProps({
     <div class="profile">
         <div class="tab-content p-4 p-md-5" >
             <div class="tab-pane fade active show" id="account" role="tabpanel" aria-labelledby="account-tab">
-                <h3 class="mb-4">Account Settings</h3>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Full Name</label>
-                            <input type="text" class="form-control" :value="user.displayName" readonly>
+                <div class="title2">
+            <h2 class="text-center">ACCOUNT SETTINGS</h2>
+        </div>
+                <div class="form_main">
+                    <div class="row form">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Full Name</label>
+                                <input type="text" class="form-control" :value="user.displayName" readonly>
+                            </div>
                         </div>
+                    
+                    
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Email address</label>
+                                <input type="text" class=" form-control" :value="user.email" readonly>
+                            </div>
+                        </div>
+                    
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" class="form-control" :value="user.password" >
+                            </div>
+                        </div>                    
                     </div>
-
-
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Email address</label>
-                            <input type="text" class=" form-control" :value="user.email" readonly>
-                        </div>
+                    
+                    <div class="account__btns">
+                        <button class="btn btn-dark">Update</button>
+                        <button class="btn btn-light">Cancel</button>
                     </div>
-
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label>Password</label>
-                            <input type="password" class="form-control" :value="user.password" >
-                        </div>
-                    </div>                    
-                </div>
-
-                <div class="account__btns">
-                    <button class="btn btn-dark">Update</button>
-                    <button class="btn btn-light">Cancel</button>
                 </div>
 
             </div>
@@ -49,5 +53,48 @@ const props = defineProps({
 <style scoped>
 .account__btns{
     margin: 0.5rem!important;
+}
+
+.btn-dark{
+    margin-right: 2rem !important;
+}
+.title2{
+    background-image: url(../assets/background-img-color.jpg);
+    padding: 2rem 1rem 2rem 1rem;
+    margin: 1rem 5rem 4rem 5rem;
+    border-radius: 1rem;
+}
+
+.title2 h2{
+    font-weight: bold;
+    font-size: 2.2rem;
+    color: var(--color-btn);
+}
+
+.profile{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 3rem;
+}
+
+.form_main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 3rem;
+}
+
+.form{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+}
+
+label{
+    padding-bottom: 1rem;
 }
 </style>
